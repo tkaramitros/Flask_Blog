610b1a30854927555c7c3f83ba1970918e1a6b92
teo <karamitrosteo@gmail.com>
2021-11-27 21:11:38 +0200

    Added Registration and Login Forms using wtforms module and created each template with a basic stracture

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECREAT_KEY'] = '0d99419a88650292f186983395729c67'
 
 posts = [
     {
@@ -27,6 +30,16 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html',title='About')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html',title='Register',form=form )
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html',title='Login',form=form )
 
 
 if __name__ == '__main__':
