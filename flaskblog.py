0b136959ce58ff730ab79f5ab3c74745f69dd11b
teo <karamitrosteo@gmail.com>
2021-11-27 18:24:04 +0200

    Defined a home page route and an about route

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 
-
 @app.route("/")
-def hello():
-    return "<h1>Hello World!</h1>"
+@app.route("/home")
+def home():
+    return "<h1>Home page!</h1>"
+
+@app.route("/about")
+def about():
+    return "<h1>About page!</h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
