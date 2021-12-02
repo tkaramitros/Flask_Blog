71fa92cc0e4672d0d15e17db2cf2d29230beeb38
teo <karamitrosteo@gmail.com>
2021-12-02 19:26:37 +0200

    Added logout route and current_user from flask_login

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
-from flask_login import login_user
+from flask_login import login_user, current_user, logout_user
 
 
 posts = [
@@ -34,6 +34,8 @@ def about():
 
 @app.route("/register", methods=['GET','POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -46,6 +48,8 @@ def register():
 
 @app.route("/login", methods=['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():       
         user = User.query.filter_by(email=form.email.data).first()
@@ -55,3 +59,8 @@ def login():
         else:
             flash('Login Unsuccessful.Please check email and password', 'danger')
     return render_template('login.html',title='Login',form=form )
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
